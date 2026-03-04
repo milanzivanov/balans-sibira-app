3590f9e30a05fd20cff807d232ee1d72b3f99cc3
milan <milanzivanov@users.noreply.github.com>
2026-03-04 11:29:29 +0100

    modified: live.ts, usecDN FALSE

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -5,5 +5,8 @@ import { defineLive } from "next-sanity/live";
 import { client } from "./client";
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client: client.withConfig({ token: process.env.SANITY_API_READ_TOKEN })
+  client: client.withConfig({
+    token: process.env.SANITY_API_READ_TOKEN,
+    useCdn: false
+  })
 });

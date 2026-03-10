aa8a88456b46f49fb19ff5ccf96903a8c0065a27
milan <milanzivanov@users.noreply.github.com>
2026-03-10 22:59:32 +0100

    speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { getLocale } from "next-intl/server";
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -27,7 +30,10 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className={inter.variable} suppressHydrationWarning>
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
